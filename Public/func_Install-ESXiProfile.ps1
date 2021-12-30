7fdd173e367abd34f91a37d87415552569a5a09c
Malcolm Yarbrough <twitch.yarby@gmail.com>
2021-12-29 19:12:58 -0600

    I give up testing if the files exist - also updated function metadata

--- Public/func_Install-ESXiProfile.ps1
+++ Public/func_Install-ESXiProfile.ps1
@@ -1,17 +1,19 @@
 function Install-ESXiProfile {
     <#
     .Synopsis
-        Install a new Image Profile to the specified ESXi host from a local datastore
+        Install or applies an image profile from a depot to this host
     .Description
-        This command is the PowerCLI equivalent of executing the shell command: esxcli software profile install
-        You can specify either a Local or a Remote Depot, but an error will be thrown if you select both.
-        If selecting a Local Depot, use the absolute path from root and use the Datastore name, not it's GUID.
+        Installs or applies an image profile from a depot to this host.
+        This command completely replaces the installed image with the image defined by the new image profile, and may result in the loss of installed VIBs.
+        The common vibs between host and image profile will be skipped. To preserve installed VIBs, use profile update instead.
+        WARNING: If your installation requires a reboot, you need to disable HA first.
 
     .PARAMETER VMHost
         The VMHost instance for the host being updated
     
     .PARAMETER Depot
-        Specifies full remote URL of the depot index.xml or server file path pointing to an offline bundle .zip file.
+        Specifies full remote URL of the depot index.xml or file path pointing to an offline bundle .zip file on a local datastore.
+        Use vmstore:\DatastoreName\path\depot.zip OR https://URL/index.xml
 
     .PARAMETER Dryrun
         Performs a dry-run only. Report the VIB-level operations that would be performed, but do not change anything in the system.
@@ -70,51 +72,6 @@ function Install-ESXiProfile {
         [string]$ImageProfile,
         [string]$Proxy = $null
     )
-
-    Write-Host "Validating inputs"
-
-    #Validation - Check if depot is remote or local
-    $ext = $Depot.Substring($Depot.Length - 3)
-    if ($ext -eq "zip") {
-
-        #Check if the LocalDepot file exists
-        if ($null -ne $LocalDepot) {
-            #Split the path so we can extract the datastore name
-            $splitpath = $LocalDepot -Split '/'
-            $ds = Get-Datastore -Name $splitpath[3] -ErrorAction Ignore
-
-            if ($null -eq $ds) {
-                throw [System.Exception] "Local Depot not found. Cannot locate datastore " + $ds
-            }
-
-            #Get the vmstore path for the datastore
-            $dsPath = $ds.DatastoreBrowserPath
-
-            #Build the vmstore path
-            $depotPath = $dsPath
-            for ($i = 4; $i -lt $splitpath.Count; $i++){
-                $depotPath = $depotPath + "\\" + $splitpath[$i]
-            }
-
-            #Use the vmstore path to test if the depot file exists
-            if (Test-Path $depotPath -ne 'True')
-            {
-                throw [System.IO.FileNotFoundException] "Local depot not found. " + $LocalDepot
-            }
-        }
-    }else {
-        if ($ext -eq "xml") {
-            #Check if RemoteDepot is reachable
-            if ($null -ne $RemoteDepot) {
-                $statuscode = Invoke-WebRequest $RemoteDepot | Select-Object statuscode
-                if ($statuscode -ne 200) {
-                    throw [System.Exception] "Remote Depot is not reachable. " + $RemoteDepot
-                }
-            }
-        }else{
-            throw [System.Exception] "Depot must either point to the index.xml of an online depot or the .zip of an offline bundle."
-        }
-    }
 
     try {
         $VMHostESXCLI = Get-EsxCli -V2 -VMHost $VMHost -ErrorAction:Stop
